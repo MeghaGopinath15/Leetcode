98a0cb7e98342900bed08014d9e4b9cf839b031c
MeghaGopinath15 <115568441+MeghaGopinath15@users.noreply.github.com>
2023-11-05 02:40:29 +0530

    Time: 753 ms (16.34%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,13 @@
-select  E.name as name from Employee E
+/*select  E.name as name from Employee E
 where E.id IN(
 select E.managerId
 from Employee E
 where E.managerId IS NOT NULL 
 group by E.managerId
-having count(E.managerId)>=5);
+having count(E.managerId)>=5); */
+
+select E.name from Employee E
+join Employee M
+on E.id = M.managerId
+group by M.managerId
+having count(*) >=5;
